c296ecc748fd8802745a5a8546191d3387dcbfc0
Kilannin Krysiak <kkrysiak@genome.wustl.edu>
2016-02-02 17:46:29 -0600

    Working version, outputs list of variants

--- lym_normal_filtering.pl
+++ lym_normal_filtering.pl
@@ -3,7 +3,9 @@ use strict;
 use warnings;
 
 ## Open readcount file
-open(NORM, "/gscmnt/gc2547/mardiswilsonlab/kkrysiak/lymphoma/variant_files/lym_normal_filter/lym_normal_indel_readcounts.tsv") or die "Input file not found";
+#open(NORM, "/gscmnt/gc2547/mardiswilsonlab/kkrysiak/lymphoma/variant_files/lym_normal_filter/lym_normal_indel_readcounts.tsv") or die "Input file not found";
+open(NORM, "/gscmnt/gc2547/mardiswilsonlab/kkrysiak/lymphoma/variant_files/lym_normal_filter/test.tsv") or die "Test file not found";
+
 
 ## Create output file
 open(OUT, "/gscmnt/gc2547/mardiswilsonlab/kkrysiak/lymphoma/variant_files/lym_normal_filter/lym_normal_output.tsv");
@@ -12,8 +14,6 @@ open(OUT, "/gscmnt/gc2547/mardiswilsonlab/kkrysiak/lymphoma/variant_files/lym_no
 my $VAF = 5; ## minimum VAF
 my $readcount = 3; ## minimum readcount
 my $recurrence = 3; ## minimum recurrence count
-
-
 
 ## Identify variants to be removed
 while(my $line = <NORM>) {
@@ -25,14 +25,16 @@ while(my $line = <NORM>) {
         my $variant = join("\t",@var[0..4]);
         my $count = 0;
         ## Iterate through the normal samples for each variant
-        for($v=6; $v<@var; $v+=3) {
+        for(my $v=7; $v<@var; $v+=3) {
+            if($var[$v] eq "NA" || $var[$v-1] eq "NA") {
+                print "WARNING: Data contains NA values\n";
             ## Check the VAF and variant readcount for each normal sample, summarizing the number of samples
-            if($var[$v]>=$VAF && $var[$v-1]>=$readcount) {
-                $count += 1;
+            } elsif($var[$v]>=$VAF && $var[$v-1]>=$readcount) {
+                $count+=1;
             }
         }
         if($count>=$recurrence){
-            print $variant;
+            print "$variant\n";
         }
     }
 }
