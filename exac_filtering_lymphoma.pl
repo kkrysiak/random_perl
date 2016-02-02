12fc8d1a7072b343b54168cef19d64e015b54b23
Kilannin Krysiak <kkrysiak@genome.wustl.edu>
2016-02-02 00:32:32 -0600

    Successfully identifies indels and represents each variant reduced both starting with prefix or starting with suffix removal

--- exac_filtering_lymphoma.pl
+++ exac_filtering_lymphoma.pl
@@ -66,6 +66,7 @@ while(my $line = <EXAC>){  ## read single line from the file
             ## Initialize the final ExAc variable and a counter
             my $exac = "";
             my $i = 0;
+            my $exac2 = "";
             
             ## Loop through all alternate alleles
             while(exists $alt[$i]){
@@ -76,71 +77,85 @@ while(my $line = <EXAC>){  ## read single line from the file
 
                 ## Adjust ref/var to match our formatting as well as start position
                 if(length($new_ref)>1 || length($new_var)>1) {
+                    ## Create a set of strings for comparison
+                    my $new_ref2 = $new_ref;
+                    my $new_var2 = $new_var;
+                    my $pos2 = $pos;
+
                     ## Remove matching suffixes
                     while(substr($new_ref,(length($new_ref)-1)) eq substr($new_var,(length($new_var)-1))) {
                         chop($new_ref);
                         chop($new_var);
-                        my $a = substr($new_ref,(length($new_ref)-1));
-                        my $b = substr($new_var,(length($new_var)-1));
-#                        print "$vars[3]\t$alt[$i]\t$a\t$b\t$new_ref\t$new_var\n";
-#                        print "$vars[3]\t$alt[$i]\t$vars[1]\tfirst:$new_ref\t$new_var\t$pos\n";
                     }
                     ## Remove matching prefixes and iterate coordinate start position
                     while(substr($new_ref,0,1) eq substr($new_var,0,1)) {
-                        ## Replace reference with 0 for insertions and iterate start
+                        ## Replace reference with 0 for insertions
                         if(length($new_ref)==1 && length($new_var)>1) {
                             $new_ref = 0;
-                            $pos = $pos + 1;
                             $new_var = substr($new_var,1,length($new_var));
                         ## Replace variant with 0 for deletions 
                         } elsif(length($new_ref)>1 && length($new_var)==1) {
                             $new_ref = substr($new_ref,1,length($new_ref));
-                            $pos = $pos + 1;   
                             $new_var = 0;
+                            $pos = $pos + 1;
                         ## Remove shared starting bases 
                         } elsif(length($new_ref)>1 && length($new_var)>1) {
                             $new_ref = substr($new_ref,1,length($new_ref));
-                            $pos = $pos + 1;
                             $new_var = substr($new_var,1,length($new_var));
+                            $pos = $pos + 1;
                         }
+                        ## Increase the starting base position
 #                        print "$vars[3]\t$alt[$i]\t$vars[1]\tsecond:$new_ref\t$new_var\t$pos\n";
                     }
-#                    print "$vars[3]\t$alt[$i]\t$a\t$b\t$new_ref\t$new_var\n";
                     $exac = join("\t",$vars[0],$pos,$new_ref,$new_var);
 #                    print "$vars[3]\t$alt[$i]\t$vars[1]\t\t$exac\n";
 
+                    ## Determine if there is another way to represent the variant
+                    ## Remove matching prefixes and iterate coordinate start position as necessary (first this time)
+                    while(substr($new_ref2,0,1) eq substr($new_var2,0,1)) {
+                        ## Replace reference with 0 for insertions
+                        if(length($new_ref2)==1 && length($new_var2)>1) {
+                            $new_ref2 = 0;
+                            $new_var2 = substr($new_var2,1,length($new_var2));
+                        ## Replace variant with 0 for deletions 
+                        } elsif(length($new_ref2)>1 && length($new_var2)==1) {
+                            $new_ref2 = substr($new_ref2,1,length($new_ref2));
+                            $new_var2 = 0;
+                            $pos2 = $pos2 + 1;
+                        ## Remove shared starting bases 
+                        } elsif(length($new_ref2)>1 && length($new_var2)>1) {
+                            $new_ref2 = substr($new_ref2,1,length($new_ref2));
+                            $new_var2 = substr($new_var2,1,length($new_var2));
+                            $pos2 = $pos2 + 1;
+                        }
+                        ## Increase the starting base position
+                    }
+                    ## Remove matching suffixes
+                    while(substr($new_ref2,(length($new_ref2)-1)) eq substr($new_var2,(length($new_var2)-1))) {
+                        if(length($new_ref2)==1 && length($new_var2)>1) {    
+                            $new_ref2 = 0;
+                            chop($new_var2);
+                        } elsif(length($new_ref2)>1 && length($new_var2)==1) {
+                            chop($new_ref2);
+                            $new_var2 = 0;
+                        } elsif(length($new_ref2)>1 && length($new_var2)>1) {
+                            chop($new_ref2);
+                            chop($new_var2);
+                        }
+                    }
+                    ## Create a second exac string using the reverse order
+                    $exac2 = join("\t",$vars[0],$pos2,$new_ref2,$new_var2);
 
+                    ## Check if the 2 exac strings are different
+                    if($exac eq $exac2) {
+                        $exac2 = "";
+                    }
 
-#                    if(length($new_ref)==1 && length($new_var)>1) {
-#                        $new_ref = 0;
-#                        my $c = substr($new_var,1,length($new_var));
-#                        print "$vars[3]\t$alt[$i]\t$new_ref\t$c\n";
-#                    }
                 } else {
+                    ## For variants with only 1 ref and 1 var base, create the exac string
                     $exac = join("\t",$vars[0],$vars[1],$vars[3],$alt[$i]);
                 }
 
-#                if(length($vars[3])>1 || length($alt[$i])>1) {
-#                    if(substr($vars[3],(length($vars[3])-1)) eq substr($alt[$i],(length($alt[$i])-1))) {
-#                        chop($
-#                        my $a = substr($vars[3],(length($vars[3])-1));
-#                        my $b = substr($alt[$i],(length($alt[$i])-1));
-#                        print "$vars[3]\t$alt[$i]\t$a\t$b\n";
-#                    }
-#                }
-#                ## Above we made the ref/var alleles 0 for indels when creating the hash keys
-#                ## Loop through all alternative alleles for a given variant
-#                if(length($alt[$i])>1) {
-#    ## TO DO: Handle 2bp reference alleles - for example chr 1:2234874 CG  C,CGG,TG 
-#                    ## For alternate alleles that are more than 1 base, replace the reference allele with 0 (insertions)
-#                    $exac = join("\t",$vars[0],$vars[1],$ins,$alt[$i]);
-#                } elsif(length($vars[3])>1) {
-#                    ## For reference alleles that are more than 1 base, replace the alternate allele with 0 (deletion)
-#                    $exac = join("\t",$vars[0],$vars[1],$vars[3],$ins);
-#                } else {
-#                    $exac = join("\t",$vars[0],$vars[1],$vars[3],$alt[$i]);
-#                }
-#                
                 ## Use the new ExAc string to see if it matches a key in the variant hash
                 if($variants{$exac}) {
                     my @info = split(";",$vars[7]);
@@ -155,8 +170,25 @@ while(my $line = <EXAC>){  ## read single line from the file
                             ## print the original test input line and the ExAc matched chr, ref, alt and allele frequency
                             print OUT "$variants{$exac}\t$exac\t$af[$i]\n";
                         }
-                    }                  
-                } 
+                    }
+                ## Use alternate exac string to query for the test variant                  
+                } elsif($exac2 ne "") {
+                    print "$exac\n$exac2\n";
+                    if($variants{$exac2}) {
+                        my @info2 = split(";",$vars[7]);
+                        foreach my $l2 (@info2) {
+                            if ($l2 =~ /^AF/) {
+                                ## Pull the allele frequency data (AF section)
+                                ## Like alternate alleles, allele frequences are separated by a , so pull the correct AF for the alt allele
+                                $l2 =~ s/AF=//;
+                                my @af2 = split(",",$l2);
+
+                                ## print the original test input line and the ExAc matched chr, ref, alt and allele frequency
+                                print OUT "$variants{$exac2}\t$exac2\t$af2[$i]\n";
+                            }
+                        }
+                    }    
+                }
 
                 $i++;        
             } 
