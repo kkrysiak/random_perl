687f8532a84696cf6cbaad2764284526b0eb3427
Kilannin Krysiak <kkrysiak@genome.wustl.edu>
2016-05-18 18:30:20 -0500

    Change PON filter to treat <5bp indels like indels

--- PON_filtering.pl
+++ PON_filtering.pl
@@ -31,12 +31,12 @@ my $usage=<<INFO;
 
     Optional parameters
         --outfile_prefix        default="PON"   File name prefix for output files
-        --snv_min_vaf           default=2.5     Minimum VAF allowed to count a SNV as detected in a normal sample
-        --snv_min_readcount     default=3       Minimum number of variant reads to count a SNV as detected in a normal sample
-        --snv_min_recurrence    default=5       Minimum number of normal samples where a SNV is detected for it to fail this filter
-        --indel_min_vaf         default=2.5     Minimum VAF allowed to count an indel as detected in a normal sample
-        --indel_min_readcount   default=2       Minimum number of variant reads to count an indel as detected in a normal sample
-        --indel_min_recurrence  default=7       Minimum number of normal samples where an indel is detected for it to fail this filter
+        --snv_min_vaf           default=2.5     Minimum VAF allowed to count a SNV/small indel (<5bp) as detected in a normal sample
+        --snv_min_readcount     default=3       Minimum number of variant reads to count a SNV/small indel (<5bp) as detected in a normal sample
+        --snv_min_recurrence    default=5       Minimum number of normal samples where a SNV/small indel (<5bp) is detected for it to fail this filter
+        --indel_min_vaf         default=2.5     Minimum VAF allowed to count a 5+ bp indel as detected in a normal sample
+        --indel_min_readcount   default=2       Minimum number of variant reads to count a 5+ bp indel as detected in a normal sample
+        --indel_min_recurrence  default=7       Minimum number of normal samples where a 5+ bp indel is detected for it to fail this filter
 
 INFO
 
@@ -48,9 +48,9 @@ open my $var, '<', $variants or die "Variant file ($variants) not found.\n\n$usa
 print "Using SNV minimum VAF: $VAF\n";
 print "Using SNV minimum readcount cutoff: $readcount\n";
 print "Using SNV minimum recurrence count: $recurrence\n";
-print "Using indel minimum VAF: $indel_VAF\n";
-print "Using indel minimum readcount: $indel_readcount\n";
-print "Using indel minimum recurrence count: $indel_recurrence\n";
+print "Using indel (5+ bp) minimum VAF: $indel_VAF\n";
+print "Using indel (5+ bp) minimum readcount: $indel_readcount\n";
+print "Using indel (5+ bp) minimum recurrence count: $indel_recurrence\n";
 
 ## Create output files
 open my $out_pass, '>', join("", $prefix, ".pass.tsv"); 
@@ -76,17 +76,33 @@ while(my $line = <$norm>) {
         } else {
             ## Check if the variant is an indel
             if($var[3] =~ /-|0/ || $var[4] =~ /-|0/) {
-                ## Iterate through readcount columns, checking VAF and variant readcount
-                for(my $vi=7; $vi<@var; $vi+=3) {
-                    if($var[$vi]>=$indel_VAF && $var[$vi-1]>=$indel_readcount) {
-                        $indel_count+=1;
+                ## Check if the variant is a small indel (<5bp)
+                if(length($var[3])<5 && length($var[4])<5){
+                    ## Iterate through readcount columns, checking VAF and variant readcount
+                    for(my $v=7; $v<@var; $v+=3) {
+                        if($var[$v]>=$VAF && $var[$v-1]>=$readcount) {
+                            $count+=1;
+                        }
                     }
-                }
-                ## Assign variant to pass or fail hash
-                if($indel_count>=$indel_recurrence){
-                    $fail{$variant} = $indel_count;
+                    ## Assign variant to pass or fail hash
+                    if($count>=$recurrence){
+                        $fail{$variant} = $count;
+                    } else {
+                        $pass{$variant} = $count;
+                    }
                 } else {
-                    $pass{$variant} = $indel_count;
+                    ## Iterate through readcount columns, checking VAF and variant readcount
+                    for(my $vi=7; $vi<@var; $vi+=3) {
+                        if($var[$vi]>=$indel_VAF && $var[$vi-1]>=$indel_readcount) {
+                            $indel_count+=1;
+                        }
+                    }
+                    ## Assign variant to pass or fail hash
+                    if($indel_count>=$indel_recurrence){
+                        $fail{$variant} = $indel_count;
+                    } else {
+                        $pass{$variant} = $indel_count;
+                    }
                 }
             ## Check if the variant is a SNV
             } elsif($var[3] =~ /^(A|C|T|G)$/ && $var[4] =~ /^(A|C|T|G)$/) {
