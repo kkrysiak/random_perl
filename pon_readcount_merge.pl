c517bd57208009d7760285a9d61077a63a93b0f3
Kilannin Krysiak <kkrysiak@genome.wustl.edu>
2016-09-09 12:13:46 -0500

    updates to usages

--- pon_readcount_merge.pl
+++ pon_readcount_merge.pl
@@ -35,35 +35,68 @@ my %hash;
 #my $filename = join("/",$dirname,"H_LS-A1-A0SB-10B-01D-A142-09_readcounts.tsv");
 #open my $file1, '<', $filename or die "Couldn't open File 1 ($filename)\n";
 
-open my $file1, '<', join("/",$dirname,"H_LS-A1-A0SB-10B-01D-A142-09_readcounts.tsv") or die "Couldn't open File 1.\n";
-open my $file2, '<', join("/",$dirname,"H_LS-A1-A0SD-10A-01D-A110-09_readcounts.tsv") or die "Couldn't open File 2.\n";
+#open my $file1, '<', join("/",$dirname,"H_LS-A1-A0SB-10B-01D-A142-09_readcounts.tsv") or die "Couldn't open File 1.\n";
+#open my $file2, '<', join("/",$dirname,"H_LS-A1-A0SD-10A-01D-A110-09_readcounts.tsv") or die "Couldn't open File 2.\n";
 
-while (my $line = <$file1>) {
-    chomp($line);
-    my @row = split("\t", $line);
-    my $ref = join("-",@row[0..4]);
-    $hash{$ref} = $line;
-}
+my $firstfile = "";
+#my $file1 = "";
 
-## Create a hash to skip duplicates and counter to count them
-my %dup;
-my $counter = 0;
+opendir(my $dir, $dirname) or die "Couldn't open directory ($dirname).\n";
 
-while (my $line2 = <$file2>) {
-    chomp($line2);
-    my @row2 = split("\t", $line2);
-    my $check = join("-",@row2[0..4]);
-    if($dup{$check}) {
-        $counter++;
+while (my $file = readdir($dir)) {
+    
+    ## check that it ends in .tsv
+    next unless ($file =~ m/\.tsv$/);
+
+    ## Get the variant list from the first file
+    if($firstfile eq "") {
+        ## Assign a value to first file to make this if statement false
+        $firstfile = $file;
+        ## Open the first file 
+        open my $file1, '<', join("/",$dirname,$file) or die "Couldn't open file ($file).\n";
+        while (my $line = <$file1>) {
+            chomp($line);
+            ## Split the first row into an array
+            my @row = split("\t", $line);
+            ## Create a hash reference out of the chromosome name, start, stop, ref, var joined by -
+            my $ref = join("-",@row[0..4]);
+            ## Assign the line to the value of the hash
+            $hash{$ref} = $line;
+        }
+
+    ## Read in the rest of the readcount files
     } else {
-        $hash{$check} = join("\t",$hash{$check},@row2[5..7]);
-        $dup{$check} = "exists";
+        ## Create a hash to skip duplicates and counter to count them
+        my %dup;
+        my $counter = 0;
+        ## Open the next file in the directory
+        open my $file2, '<', join("/",$dirname,$file) or die "Couldn't open file ($file).\n";
+        while (my $line2 = <$file2>) {
+            chomp($line2);
+            my @row2 = split("\t", $line2);
+            ## Make the variant (chr, start, stop, ref, var) a key to check if it is in the hash
+            my $check = join("-",@row2[0..4]);
+            ## Check if that variant has been seen before (therefore a duplicate in this file
+            if($dup{$check}) {
+                ## Increment the duplicate counter
+                $counter++;
+            ## If it is not a duplicate add it to the hash
+            } else {
+                ## Appends the 3 readcount columns to the end of the growing hash
+                $hash{$check} = join("\t",$hash{$check},@row2[5..7]);
+                ## Add the variant to the duplicate check hash
+                $dup{$check} = "exists";
+            }
+        }
+        #print "$_ $hash{$_}\n" for (keys %hash);
+        print "$counter duplicates $file\n";
     }
+    close $file;
 }
 
-print "$_ $hash{$_}\n" for (keys %hash);
-print "$counter duplicates\n";
+print $out "$hash{$_}\n" for (keys %hash);
 
-close $file1;
-close $file2;
+#close $file;
+#close $file1;
+#close $file2;
 close $out;
